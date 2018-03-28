03be169529387ea1e83a725ebed74b1b0690a16e
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-28 12:22:22 -0700

    Use an array instead of an std::list

--- Lab 8/Lab 8/FixedList.hpp
+++ Lab 8/Lab 8/FixedList.hpp
@@ -1,22 +1,60 @@
 #pragma once
 #include <iostream>
-#include <list>
+#include <array>
 
 
-using namespace std;
-
-template<class T, size_t N>
+template<class T = string, size_t N = 10>
 class FixedList {
 public:
-	FixedList();
-	~FixedList();
-	const T& get(unsigned int index) const;
-	T& operator[] (unsignes int index);
-	int getFirstIndex(const T& t) const;
-	size_t size()const;
-	size_t capacity() const;
-	bool add(const T& t);
-	T remove(const T& t);
+	FixedList()
+	{
+		std::cout << "made a list class" << endl;
+		
+	}
+	
+	~FixedList() 
+	{
+		//no dynamic memory so don't do anything here
+	}
+
+	const T& get(unsigned int index) const
+	{
+	
+	}
+	T& operator[](unsigned int index)
+	{
+		//int iter = thisList.begin();
+		//std::advance(iter, index);
+		//int x = *iter;
+		//return *iter;
+	}
+	int getFirstIndex(const T& t) const {
+
+	}
+	size_t size()const
+	{
+
+	}
+	size_t capacity() const {
+
+
+	}
+	bool add(const T& t)
+	{
+
+	}
+	T remove(const T& t)
+	{
+
+	}
+
+	//keep public for testing
+	T contents[N];
+
 private:
 	//private variables here
+	
+	
+
+	//void shiftLeft(T& startIndex)
 };
---
+++ Lab 8/Lab 8/FixedListTester.cpp
@@ -0,0 +1,12 @@
+#include "FixedList.hpp"
+using namespace std;
+
+int main() {
+
+	FixedList<int, 5> testList{};
+
+
+
+	cin.get();
+
+
