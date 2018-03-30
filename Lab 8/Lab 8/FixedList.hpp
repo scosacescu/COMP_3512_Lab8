4ccc5985027f695a91af30a19e856929b084d726
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-30 16:17:50 -0700

    add(), get(), and capacity()

--- Lab 8/Lab 8/FixedList.hpp
+++ Lab 8/Lab 8/FixedList.hpp
@@ -1,26 +1,32 @@
 #pragma once
 #include <iostream>
 #include <array>
+#include <typeinfo>
 
 
 template<class T = string, size_t N = 10>
 class FixedList {
 public:
+	// Zero Param constructor. Type and size to be specified via template params
 	FixedList()
 	{
-		std::cout << "made a list class" << endl;
+		std::cout << "made a FixedList class with type = " << typeid(T).name() << " and size = "<< maxCapacity << endl;
 		
 	}
 	
-	~FixedList() 
-	{
-		//no dynamic memory so don't do anything here
-	}
+	// No dynamic memory used here, so don't mind me
+	~FixedList() {}
 
+	//Access to elements stored in your FixedList 
+	//PRE: Specify the index of the element you want in your list
+	//POST: Element stored at that index will be returned, if valid
 	const T& get(unsigned int index) const
 	{
-	
+		cout << "The element stored at index " << index << " is " << contents[index] << endl;
+		return contents[index];
 	}
+	
+	// Overload the [] operator just for funsies. This works the same as get(index) above
 	T& operator[](unsigned int index)
 	{
 		//int iter = thisList.begin();
@@ -28,33 +34,71 @@ public:
 		//int x = *iter;
 		//return *iter;
 	}
+	//
+	//PRE: Specify the element to search for in the FixedList
+	//POST: The index of the first occurence of that element returned, else -1
 	int getFirstIndex(const T& t) const {
-
+		return 1;
 	}
+	
+	//PRE: a FixedList object
+	//POST: The actual number of elements in the FixedList's contents[]
 	size_t size()const
 	{
+		return 1;
 
 	}
+
+	//PRE: a FixedList object
+	//POST: The maximum number of elements in the FixedList's container[]
 	size_t capacity() const {
-
-
+		cout << "The maximum capacity of your FixedList is: " << maxCapacity << endl;
+		return maxCapacity;
 	}
+
+	//PRE:  Add the specified element to the FixedLists' contents[], space permitting
+	//POST: True on success, else false
 	bool add(const T& t)
 	{
-
+		if (counter <= static_cast<int>(maxCapacity)) {
+			contents[counter] = t;
+			cout << "Inserted value " << t << " at index " << counter << endl;
+			counter++;
+			return true;
+		}
+		else
+		{
+			cout << "Sorry, list full! Unable to insert value " << t << endl;
+			return false;
+		}
 	}
+
+	//PRE: Remove the first occurence of the specified element from the FixedList's contents[]
+	//POST: If found, the element will be returned and all following elements will be shifted one left. 
 	T remove(const T& t)
 	{
+		for (size_t i = 0; i < 10; i++) {
+			if (contents[i] == t){
+				//remove
+				//shiftLeft(i);
+				break;
+			}
+			else {
+				return -1;
+			}
+		}
 
 	}
 
-	//keep public for testing
-	T contents[N];
+
+	static constexpr size_t maxCapacity = N; //assign the max capacity through template params
+	T contents[maxCapacity]; //this is the "fixed list" our FixedList class is built to manage
+	int counter = 0;
 
 private:
-	//private variables here
+	
 	
 	
 
-	//void shiftLeft(T& startIndex)
+	//void shiftLeft(T startIndex)
 };
--- Lab 8/Lab 8/FixedListTester.cpp
+++ Lab 8/Lab 8/FixedListTester.cpp
@@ -3,10 +3,36 @@ using namespace std;
 
 int main() {
 
+	//test objects
 	FixedList<int, 5> testList{};
+	FixedList<> testList2{};
+	FixedList<int, 5> testList3;
 
+	//make sure passing template params works
+	cout << testList.maxCapacity << endl;
+	cout << testList2.maxCapacity << endl;
+
+	//check capacity method is correct
+	testList.capacity();
+	testList2.capacity();
+	testList3.capacity();
+
+	// check add()
+	testList3.add(0);
+	testList3.add(1);
+	testList3.add(2);
+	testList3.add(3);
+	testList3.add(4);
+	testList3.add(5);
+	testList3.add(6);
+
+	// test retrieval of values at specified indices
+	testList3.get(0);
+
+
+	
 
 
 	cin.get();
-
+}
 
