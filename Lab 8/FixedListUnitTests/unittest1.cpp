42c91c8f501f747a40041c65486ed6ef070b802d
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-30 23:53:13 -0700

    Add some unit tests

--- Lab 8/FixedListUnitTests/unittest1.cpp
+++ Lab 8/FixedListUnitTests/unittest1.cpp
@@ -3,16 +3,69 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+using namespace std;
+
 namespace FixedListUnitTests
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(Add)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(AddInts)
 		{
-			// TODO: Your test code here
+			//Arrange
+			FixedList<int, 3> l1;
+			//Act
+			l1.add(10);
+			//Assert
+			Assert::AreEqual(10, l1[0]);
+		}
+
+		TEST_METHOD(AddIntsToFull)
+		{
+			FixedList<int, 1> l2;
+			l2.add(0);
+			bool result = l2.add(1);
+			Assert::AreEqual(result, false);
+		}
+
+		TEST_METHOD(AddStringToFull)
+		{
+			FixedList<string, 1> l4;
+			l4.add("Steph");
+			bool result = l4.add("Hello");
+			Assert::AreEqual(result, false);
 		}
 
 	};
+
+	TEST_CLASS(Remove)
+	{
+	public:
+
+		TEST_METHOD(RemoveInts)
+		{
+			//Arrange
+			FixedList<int, 3> list;
+			//Act
+			list.add(10);
+			int result = list.remove(10);
+			//Assert
+			Assert::AreEqual(result, 10);
+		}
+
+		TEST_METHOD(RemoveString)
+		{
+			////Arrange
+			//FixedList<string, 3> list;
+			////Act
+			//list.add("10");
+			//string result = list.remove("10");
+			////Assert
+			//Assert::AreEqual(result, "10");
+		}
+
+	};
+
+
 }
