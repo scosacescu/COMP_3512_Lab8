ba528f138e5500bf3056de0258ec74d8ec04c7c4
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-30 23:24:31 -0700

    Add Unit Test project

---
+++ Lab 8/FixedListUnitTests/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "CppUnitTest.h"
+#include "../Lab 8/FixedList.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FixedListUnitTests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
